355ee35927df18ed2d14f12559f0cf9cbd4244fa
Afeez Babatunde <afeezbabatunde@gmail.com>
2022-09-08 07:49:01 -0700

    and that piece of art is useful

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,17 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
-* main - Print and that piece of art is useful"
+* main - print and that piece of art is useful" - Dora Korpar, 2015-10-19
 *
-* Description: Programing in C
+* Description: Programming in C
 *
 * Return: 1
 */
 
 int main(void)
 {
-write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+59);
 return (1);
 }
