197d75b1d95539d281fb1997b538bb53a8163858
Afeez Babatunde <afeezbabatunde@gmail.com>
2022-09-08 06:13:02 -0700

    prints Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 
+
+
 /**
-* Prints "Programming is like building a multilingual puzzle.
-*
-* Return 0
-*/
+
+ * Prints "Programming is like building a multilingual puzzle.
+
+ *
+
+ *
+
+ * Return: Always 0.
+
+ */
+
+
+
 int main(void)
+
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
-}
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+
+} 
