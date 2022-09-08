17ccb9cddc4bd977a7fbcd89271731c92d02c673
Afeez Babatunde <afeezbabatunde@gmail.com>
2022-09-08 07:33:25 -0700

    and that piece of art is useful

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Print and that piece of art is useful"
+*
+* Description: Programing in C
+*
+* Return: 1
+*/
+
+int main(void)
+{
+print("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
