9e11a5db4508396a11ce9ca05ed8c64d018af2e7
Afeez Babatunde <afeezbabatunde@gmail.com>
2022-09-08 05:18:56 -0700

    C program to print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>                                                                                                                                                                              
+
+/* Print out text in C */                                                                                                                                                                       
+
+                                                                                                                                                                                                
+
+int main() {                                                                                                                                                                                    
+
+                                                                                                                                                                                                
+
+puts("\"Programming is like building a multilingual puzzle");                                                                                                                                  
+
+return(0)                                                                                                                                                                                       
+
+}
+
