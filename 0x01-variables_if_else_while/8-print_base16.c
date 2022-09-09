32b884f8535bd1b5ba8a894504bac6305dda8f20
Afeez Babatunde <afeezbabatunde@gmail.com>
2022-09-10 01:20:30 +0200

    Prints all the numbers of base 16 in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,12 +8,11 @@
 int main(void)
 {
 int num;
-char letter;
-
+char c;
 for (num = 0; num < 10; num++)
 putchar((num % 10) + '0');
-for (letter = 'a'; letter <= 'f'; letter++)
-putchar(letter);
+for (c = 'a'; c <= 'f'; c++)
+putchar(c);
 putchar('\n');
 return (0);
 }
