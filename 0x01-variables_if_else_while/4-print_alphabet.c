62caad824d7885ab1f6361d0aed4d1a413e6abf8
Afeez Babatunde <afeezbabatunde@gmail.com>
2022-09-10 00:44:54 +0200

    Prints the alphabet in lowercase, except for q and e

--- 0x01-variables_if_else_while/4-print_alphabet.c
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -6,14 +6,13 @@
  *
  * Return: Always 0.
  */
-int main() {
+int main(void)
+{
 char c;
-for (c = a; c <= z; ++c)
+for (c = 'a'; c <= 'z'; ++c)
 if (c != e && c != q)
 {
 printf("%c ", c);
-}        
-return 0;
 }
 return (0);
 }
