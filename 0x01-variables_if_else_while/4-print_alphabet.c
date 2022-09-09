b2508ea28b1ef63b7a71f55fb71c8a2d0345cbb9
Afeez Babatunde <afeezbabatunde@gmail.com>
2022-09-09 15:01:03 -0700

    Write a program that prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
+int main() {
+char c;
+for (c = a; c <= z; ++c)
+if (c != e && c != q)
+{
+printf("%c ", c);
+}        
+return 0;
+}
+return (0);
+}
