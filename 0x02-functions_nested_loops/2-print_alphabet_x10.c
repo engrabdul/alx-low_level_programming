f095fa93dd1dbd20e246eb909e7a684a906bb34e
Afeez Babatunde <afeezbabatunde@gmail.com>
2022-09-14 01:50:22 +0200

    update

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,7 @@
 #include main.h
 
 /**
-<<<<<<< HEAD
  * print_alphabet_x10 - Make alphabet x10 times
-=======
- * main - check the code.
->>>>>>> 6de0ffe7babfb051c579264d3b960af4fe670541
  *
  * Return: Always 0.
  */
