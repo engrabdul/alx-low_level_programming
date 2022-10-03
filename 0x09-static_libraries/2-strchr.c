ea578ff4237e9bfa094375b1a32e65d408a728f4
Afeez Babatunde <afeezbabatunde@gmail.com>
2022-10-02 17:17:15 -0700

    Push all

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - reads and returns first occurence of a character c
+ *  in a string S,
+ * @s: string.
+ * @c: character.
+ * Return: the pointer to the first occurrence of the character c.
+ */
+char *_strchr(char *s, char c)
+{
+unsigned int a = 0;
+
+for (; *(s + a) != '\0'; a++)
+if (*(s + a) == c)
+return (s + a);
+if (*(s + a) == c)
+return (s + a);
+return ('\0');
+}
